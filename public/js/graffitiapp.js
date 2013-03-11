065e137f61300cbfe950df6af2c8a06574cd0ee6
Justin K. Chen <justinkchen@stanford.edu>
2013-03-11 00:20:26 -0700

    updated graffiti js

--- public/js/graffitiapp.js
+++ public/js/graffitiapp.js
@@ -7,20 +7,6 @@ $(function() {
     var info = $("#infobox");
     var doc = $(document);
  
-    // custom marker's icon styles
-    var tinyIcon = L.Icon.extend({
-        options: {
-            shadowUrl: "../assets/marker-shadow.png",
-            iconSize: [25, 39],
-            iconAnchor:   [12, 36],
-            shadowSize: [41, 41],
-            shadowAnchor: [12, 38],
-            popupAnchor: [0, -30]
-        }
-    });
-    var redIcon = new tinyIcon({ iconUrl: "../assets/marker-red.png" });
-    var yellowIcon = new tinyIcon({ iconUrl: "../assets/marker-yellow.png" });
- 
     var sentData = {}
  
     var connects = {};
@@ -54,20 +40,7 @@ $(function() {
             icon: redIcon
         });
  
-        // load leaflet map
-        map = L.map('map', {
-    			center: [lat, lng],
-    			zoom: 16 
-	});
 
-        // leaflet API key tiler
-        L.tileLayer("http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png", { maxZoom: 18, detectRetina: true }).addTo(map);
-         
-        // set map bounds
-        // map.fitWorld();
-        userMarker.addTo(map);
-        userMarker.bindPopup("<p>You are here! Your ID is " + userId + "</p>").openPopup();
- 
         // send coords on when user is active
         doc.on("mousemove", function() {
             active = true; 
@@ -89,15 +62,6 @@ $(function() {
         active = false;
     });
  
-    // showing markers for connections
-    function setMarker(data) {
-        for (i = 0; i < data.coords.length; i++) {
-            var marker = L.marker([data.coords[i].lat, data.coords[i].lng], { icon: yellowIcon }).addTo(map);
-            marker.bindPopup("<p>One more external user is here!</p>");
-            markers[data.id] = marker;
-        }
-    }
- 
     // handle geolocation api errors
     function positionError(error) {
         var errors = {
@@ -111,14 +75,4 @@ $(function() {
     function showError(msg) {
         info.addClass("error").text(msg);
     }
- 
-    // delete inactive users every 15 sec
-    setInterval(function() {
-        for (ident in connects){
-            if ($.now() - connects[ident].updated > 15000) {
-                delete connects[ident];
-                map.removeLayer(markers[ident]);
-            }
-        }
-    }, 15000);
 });
